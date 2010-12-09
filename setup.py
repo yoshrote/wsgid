80aae38f0b104f68ae6a352a487d383ac3dd4dec
Dalton Barreto <daltonmatos@gmail.com>
2010-12-09 21:56:19 -0200

    Instalation files

---
+++ setup.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+from setuptools import setup
+from wsgid import __version__
+
+setup(
+  name="wsgid",
+  version=__version__,
+  url="https://github.com/daltonmatos/wsgi",
+  license="GPLv2",
+  description="A complete WSGI environment for mongrel2 handlers",
+  author="Dalton Barreto",
+  author_email="daltonmatos@gmail.com",
+  long_description=file('README').read(),
+  packages=['wsgid'],
+  classifiers = [
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Application Frameworks"
+    ])
