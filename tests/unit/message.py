f084872061a90333fde637f1edf34036162eeafd
Dalton Barreto <daltonmatos@gmail.com>
2011-01-08 16:25:47 -0200

    Done parsing of mongrel2 raw messages.

--- tests/unit/message.py
+++ tests/unit/message.py
@@ -3,16 +3,19 @@
 
 import unittest
 from wsgid.core import Message
+import json
 
 
 
 class PaserTest(unittest.TestCase):
 
+
   def setUp(self):
     self.server_id = "uuid"
     self.client_id = "42"
     self.path = "/some/path"
-    self.netstring = "7:headers,4:body," 
+    self.header_json = json.dumps({'header':'value'})
+    self.netstring = "%d:%s,4:body," % (len(self.header_json), self.header_json) 
 
 
   def test_parse_mongrel2_message(self):
@@ -25,7 +28,7 @@ class PaserTest(unittest.TestCase):
     self.assertEquals(self.netstring, parsed_message.netstring)
 
   def test_parse_body_with_space(self):
-    body_with_space = "6:header,12:body w space,"
+    body_with_space = "%d:%s,12:body w space," % (len(self.header_json), self.header_json)
     msg = "%s %s %s %s" % (self.server_id, self.client_id,\
         self.path, body_with_space)
     parsed_message = Message(msg)
@@ -35,7 +38,8 @@ class PaserTest(unittest.TestCase):
     self.assertEquals(body_with_space, parsed_message.netstring)
 
   def test_parse_headers_with_space(self):
-    headers_with_space = "15:headers w space,4:body,"
+    header_w_space = json.dumps({'name': 'value with space'})
+    headers_with_space = "%d:%s,4:body," % (len(header_w_space), header_w_space)
     msg = "%s %s %s %s" % (self.server_id, self.client_id,\
         self.path, headers_with_space)
     parsed_message = Message(msg)
@@ -43,3 +47,30 @@ class PaserTest(unittest.TestCase):
     self.assertEquals(self.client_id, parsed_message.client_id)
     self.assertEquals(self.path, parsed_message.path)
     self.assertEquals(headers_with_space, parsed_message.netstring)
+    
+  def test_parse_real_headers(self):
+    headers = {'PATH': '/some/path', 'PATTERN': '/some.*', 'host': 'localhost'}
+    headers_json = json.dumps(headers)
+    
+    netstring = "%d:%s,4:body," % (len(headers_json), headers_json)
+    
+    msg = "%s %s %s %s" % (self.server_id, self.client_id,\
+                           self.path, netstring)
+    
+    parsed_message = Message(msg)
+    
+    self.assertEqual(netstring, parsed_message.netstring)
+    self.assertEqual(headers, parsed_message.headers)
+    
+  def test_parse_real_body(self):
+    body = "<html> <body> some content </body> </html>"
+    
+    netstring = "%d:%s,%d:%s," % (len(self.header_json), self.header_json,\
+                                  len(body), body)
+    
+    msg = "%s %s %s %s" % (self.server_id, self.client_id,\
+                           self.path, netstring)
+    parsed_message = Message(msg)
+    
+    self.assertEqual(body, parsed_message.body)
+
--- wsgid/core/message.py
+++ wsgid/core/message.py
@@ -1,11 +1,25 @@
 #encoding: utf-8
 
+import json
 
 class Message(object):
   '''
     Represents a mongrel2 raw message
+    Here we have:
+     * server_id: The ID of the server that sent us this message
+     * client_id: The id of the client that must receive our response
+     * path: The path that was requested
+     * netstring: More raw data. Here is the headers and the request body
+     * headers: Mongrel headers already parsed. json -> dict
+     * body: Original request body
   '''
 
   def __init__(self, m2message):
     self.server_id, self.client_id,\
         self.path, self.netstring = m2message.split(' ', 3)
+    len_headers, rest = self.netstring.split(':', 1)
+    
+    self.headers = json.loads(rest[:int(len_headers)])
+    rest_with_body = rest[int(len_headers)+1:]
+    len_body , rest = rest_with_body.split(':', 1)
+    self.body = rest[:int(len_body)]
