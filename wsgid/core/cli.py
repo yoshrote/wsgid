2ba45107b4a8910aeb182268ca39601961dd2382
Dalton Barreto <daltonmatos@gmail.com>
2011-03-13 13:48:46 -0300

    Basic daemonizing support

--- wsgid/core/cli.py
+++ wsgid/core/cli.py
@@ -8,6 +8,8 @@ from wsgid import Wsgid
 from ..loaders import load_app
 
 import plugnplay
+import daemon
+import signal
 
 class Cli(object):
   '''
@@ -26,25 +28,44 @@ class Cli(object):
 
   def run(self):
     (original_parser, options, args) = parser.parse_args()
-    daemon = not options.nodaemon
     try:
+      self._set_loggers(options)
+
       self.validate_input_params(app_path=options.app_path,\
           uuid=options.uuid, recv=options.recv, send=options.send,\
           wsgi_app=options.wsgi_app)
 
-      self._set_loggers(options)
+      daemon_options = self._create_deamon_options(options)
 
       if options.loader_dir:
         plugnplay.set_plugin_dirs(*options.loader_dir)
         plugnplay.load_plugins()
 
-      if daemon:
-        pass # daemon code
-      else:
+      ctx = daemon.DaemonContext(**daemon_options)
+      with ctx:
         self._call_wsgid(options)
     except Exception, e:
       self.log.exception(e)
       sys.exit(1)
+
+  def _create_deamon_options(self, options):
+    daemon = {'detach_process': not options.nodaemon}
+    if options.nodaemon:
+      daemon.update({ 'stdin': sys.stdin, 
+                     'stdout': sys.stdout, 
+                     'stderr': sys.stderr})
+      # If we are not a daemon we must maintain the basic signal handlers
+      daemon.update({'signal_map': {
+          signal.SIGTTIN: signal.getsignal(signal.SIGTTIN),
+          signal.SIGTTOU: signal.getsignal(signal.SIGTTOU),
+          signal.SIGTSTP: signal.getsignal(signal.SIGTSTP),
+          signal.SIGTERM: signal.getsignal(signal.SIGTERM)
+        }})
+    else:
+      daemon.update({'signal_map':
+                      {signal.SIGTERM: signal.getsignal(signal.SIGTERM)}
+        })
+    return daemon
 
   def _call_wsgid(self, options):
     wsgi_app = load_app(options.app_path, options.wsgi_app)
